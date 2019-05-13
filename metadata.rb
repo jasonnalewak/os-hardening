4603381832455b098cdb0df4ed02ea88cbea5cba
Jason Nalewak <jnalewak@chef.io>
2019-05-12 23:08:41 -0400

    updated metadata.rb and readme

--- metadata.rb
+++ metadata.rb
@@ -6,10 +6,14 @@ description 'Installs/Configures hardening'
 long_description 'Installs/Configures hardening'
 version '0.1.0'
 chef_version '>= 12.1' if respond_to?(:chef_version)
+
+supports 'amazon'
 supports 'ubuntu', '>= 16.04'
-supports 'rhel'
+supports 'debian', '>= 9.0'
+supports 'centos', '>= 6.0'
+supports 'redhat', '>= 6.0'
 supports 'windows'
-supports 'centos'
+
 issues_url 'none'
 source_url 'https://github.com/jasonnalewak/os-hardening.git'
 
